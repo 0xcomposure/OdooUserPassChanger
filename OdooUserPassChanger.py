b76d16ada0b368542aff9ecc8e84d71b72665a1b
Jesus Cuesta <jesusdanielcf@gmail.com>
2016-10-22 01:22:47 +0100

    Update OdooUserPassChanger.py

--- OdooUserPassChanger.py
+++ OdooUserPassChanger.py
@@ -1,14 +1,21 @@
 from passlib.context import CryptContext
-import psycopg2, getpass, signal, sys
+import psycopg2
+import getpass
+import signal
+import sys
 
 """Function to print a message when Ctrl+C is pressed."""
 def handler(signum, frame):
-    print ('\nLeaving the application...\nGood bye!')
+    print('\nLeaving the application...\nGood bye!')
     sys.exit(0)
 
 signal.signal(signal.SIGINT, handler)
 
-"""variables to get DB connection data, user you want to change de password and the new password for the user."""
+"""
+variables to get DB connection data,
+user you want to change de password
+and the new password for the user.
+"""
 dbname = input("Enter the DB name: ")
 dbuser = input("Enter the DB user: ")
 dbpass = getpass.getpass("Enter the password for " + dbuser + ": ")
@@ -25,11 +32,11 @@ if the connection failed return a message.
 """
 try:
     conn = psycopg2.connect("dbname='" + dbname + "' user='" + dbuser + "' host='" + dbhost + "' password='" + dbpass + "'")
-    print ("Opened database successfully")
+    print("Opened database successfully")
     cur = conn.cursor()
     cur.execute("UPDATE res_users SET password_crypt = '"+newpass_crypt+"' WHERE login = '"+usertoupdate+"'")
     conn.commit()
     conn.close()
     print("Password for "+usertoupdate+" has been updated successfully")
 except:
-    print ("Unable to connect to the database")
+    print("Unable to connect to the database")
